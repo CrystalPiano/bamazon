6616c6fa65a97e562e77fede806a58333bc524e9
LucFII <LSFIIChi@Gmail.com>
2017-08-02 22:06:49 -0500

    updated sql to reflect new mock products

--- assets/js/bamazonCustomer.js
+++ assets/js/bamazonCustomer.js
@@ -1,5 +1,6 @@
 /* Notes:
 
+-Add mock items to data base
 -Display items for sale
 -Inquire what user wants to buy
 -Verify quantity of desired item
@@ -26,6 +27,8 @@ connection.connect(function(err) {
   createProduct();
 });
 
+
+/*
 // Adding items to the table in the bamazondb
 function createProduct() {
     console.log("Inserting new products...\n");
@@ -37,63 +40,8 @@ function createProduct() {
         price: 19.99,
         stock_quantity: 999
       },
-      {
-        product_name: "cool water bottle",
-        department_name: "fitness",
-        price: 9.99,
-        stock_quantity: 100
-      },
-            {
-        product_name: "reusable rocket",
-        department_name: "space",
-        price: 12000000.00,
-        stock_quantity: 3
-      },
-            {
-        product_name: "NASA tee shirt",
-        department_name: "space",
-        price: 14.99,
-        stock_quantity: 250
-      },
-            {
-        product_name: "dog leash",
-        department_name: "pets",
-        price: 9.99,
-        stock_quantity: 75
-      },
-            {
-        product_name: "dog food",
-        department_name: "pets",
-        price: 49.99,
-        stock_quantity: 50
-      },
-            {
-        product_name: "human food",
-        department_name: "food",
-        price: 24.50,
-        stock_quantity: 100
-      },
-            {
-        product_name: "massive square watermelon",
-        department_name: "food",
-        price: 12.99,
-        stock_quantity: 8
-      },
-            {
-        product_name: "chunk of gold",
-        department_name: "commodities",
-        price: 799.99,
-        stock_quantity: 20
-      },
-            {
-        product_name: "chunk of coal",
-        department_name: "commodities",
-        price: 4.99,
-        stock_quantity: 595
-      },
-
       function(err, res) {
-        console.log(res.affectedRows + " product inserted!\n");
+        console.log(res.affectedRows + " products inserted!\n");
         // Call updateProduct AFTER the INSERT completes
         //updateProduct();
       }
@@ -101,8 +49,11 @@ function createProduct() {
 
     // logs the actual query being run
     console.log(query.sql);
-};
+}; */
 
+
+
+/*
 function updateProduct() {
   console.log("Updating stock quantity of selected item...\n");
   var query = connection.query(
@@ -148,3 +99,4 @@ function readProducts() {
     connection.end();
   });
 };
+*/
--- assets/sql/bamazondb.sql
+++ assets/sql/bamazondb.sql
@@ -1,3 +1,5 @@
+drop database bamazondb;
+
 create database bamazondb;
 
 USE bamazondb;
@@ -10,3 +12,10 @@ CREATE TABLE products (
     stock_quantity INT NULL,
     PRIMARY KEY (item_id)
     );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Shake Weight", "fitness", 19.99, 999), ("Cool Water Bottle", "fitness", 9.99, 100),
+("Reusable Rocket", "space", 12000000.00, 3), ("NASA Tee Shirt", "space", 14.99, 250),
+("Dog Leash", "pets", 9.99, 75), ("Dog Food", "pets", 49.99, 50), ("Human Food", "food", 24.50, 100),
+("Massive Square Watermelon", "food", 12.99, 8), ("Chunk Of Gold", "commodities", 799.99, 20),
+("Chunk Of Coal", "commodities", 4.99, 595);
